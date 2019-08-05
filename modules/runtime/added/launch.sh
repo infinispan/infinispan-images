5ae120e7692043801e98c623268ab1b294ca0510
Ryan Emerson <remerson@redhat.com>
2019-08-05 17:07:30 +0100

    Basic udp clustering in place

--- modules/runtime/added/launch.sh
+++ modules/runtime/added/launch.sh
@@ -3,15 +3,19 @@
 # Entry point for the image which initiates any pre-launch config required before
 # executing the server.
 # ===================================================================================
-SERVER_ROOT=/opt/infinispan
-
 if [ -n "${DEBUG:-}" ]; then
   set -x
 fi
+
+# hostname not available with uib-minimal
+BIND=$(cat /etc/hosts | grep -m 1 $(cat /proc/sys/kernel/hostname) | awk '{print $1;}')
+SERVER_ROOT=/opt/infinispan
 
 # If $ISPN_HOME does not comply with SERVER_ROOT, then create a symlink. Necessary in order to allow overriding of location at build time
 if [ $SERVER_ROOT != $ISPN_HOME ]; then
   ln -s $SERVER_ROOT $ISPN_HOME
 fi
 
-exec $ISPN_HOME/bin/server.sh
+exec $ISPN_HOME/bin/server.sh \
+--bind-address=$BIND \
+-Dinfinispan.cluster.stack=$JGROUPS_STACK
