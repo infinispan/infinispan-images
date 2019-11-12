1c490f21060f98a0ff4e4d41995a82ee7ed25032
Ryan Emerson <remerson@redhat.com>
2019-11-12 12:23:10 +0000

    #16 Provide MANAGED_ENV env var for operator type environments

--- modules/runtime/added/bin/launch.sh
+++ modules/runtime/added/bin/launch.sh
@@ -4,10 +4,6 @@
 # executing the server.
 # ===================================================================================
 
-generate_user_or_password() {
-  echo $(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
-}
-
 printLn() {
   format='# %-76s #\n'
   printf "$format" "$1"
@@ -17,6 +13,58 @@ printBorder() {
   printf '#%.0s' {1..80}
   printf "\n"
 }
+
+generate_user_or_password() {
+  echo $(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
+}
+
+generate_identities_yaml() {
+  # If no identities file provided, then use provided user/pass or generate as required
+  if [ -z ${IDENTITIES_PATH} ]; then
+    printBorder
+    printLn
+    printLn "IDENTITIES_PATH not specified"
+    if [ -n "${USER}" ] && [ -n "${PASS}" ]; then
+      printLn "Generating Identities yaml using USER and PASS env vars."
+    else
+      USER=$(generate_user_or_password)
+      PASS=$(generate_user_or_password)
+      printLn "USER and/or PASS env variables not specified."
+      printLn "Auto generating user and password."
+      printLn
+      printLn "Generated User: ${USER}"
+      printLn "Generated Password: ${PASS}"
+      printLn
+      printLn "These credentials should be passed via environment variables when adding"
+      printLn "new nodes to the cluster to ensure that clients can access the exposed"
+      printLn "endpoints, on all nodes, using the same credentials."
+      printLn
+      printLn "For example:"
+      printLn "    'docker run -e USER=${USER} -e PASS=${PASS}''"
+      printLn
+    fi
+    printBorder
+
+identities=$(cat <<-YamlEnd
+credentials:
+  - username: ${USER}
+    password: ${PASS}
+YamlEnd
+)
+    export IDENTITIES_PATH=${ISPN_HOME}/server/conf/generated-identities.yaml
+    echo "${identities}" > ${IDENTITIES_PATH}
+  fi
+}
+
+generate_content() {
+  if [ "${MANAGED_ENV^^}" != "TRUE" ]; then
+    generate_identities_yaml
+  fi
+}
+
+# ===================================================================================
+# Script Execution
+# ===================================================================================
 
 set -e
 if [ -n "${DEBUG}" ]; then
@@ -30,43 +78,12 @@ if [ ${SERVER_ROOT} != ${ISPN_HOME} ]; then
   ln -s ${SERVER_ROOT} ${ISPN_HOME}
 fi
 
-# If no identities file provided, then use provided user/pass or generate as required
-if [ -z ${IDENTITIES_PATH} ]; then
-  printBorder
-  printLn
-  printLn "IDENTITIES_PATH not specified"
-  if [ -n "${USER}" ] && [ -n "${PASS}" ]; then
-    printLn "Generating Identities yaml using USER and PASS env vars."
-  else
-    USER=$(generate_user_or_password)
-    PASS=$(generate_user_or_password)
-    printLn "USER and/or PASS env variables not specified."
-    printLn "Auto generating user and password."
-    printLn
-    printLn "Generated User: ${USER}"
-    printLn "Generated Password: ${PASS}"
-    printLn
-    printLn "These credentials should be passed via environment variables when adding"
-    printLn "new nodes to the cluster to ensure that clients can access the exposed"
-    printLn "endpoints, on all nodes, using the same credentials."
-    printLn
-    printLn "For example:"
-    printLn "    'docker run -e USER=${USER} -e PASS=${PASS}''"
-    printLn
-  fi
-  printBorder
+generate_content
 
-identities=$(cat <<-YamlEnd
-credentials:
-  - username: ${USER}
-    password: ${PASS}
-YamlEnd
-)
-  export IDENTITIES_PATH=${ISPN_HOME}/server/conf/generated-identities.yaml
-  echo "${identities}" > ${IDENTITIES_PATH}
-fi
+[[ -n ${IDENTITIES_PATH} ]] && IDENTITES_OPT="--identities=${IDENTITIES_PATH}"
+[[ -n ${CONFIG_PATH} ]] && CONFIG_OPT="--config=${CONFIG_PATH}"
 
-java -jar ${ISPN_HOME}/bin/config-generator.jar ${ISPN_HOME}/server/conf ${IDENTITIES_PATH} ${CONFIG_PATH}
+java -jar  ${ISPN_HOME}/bin/config-generator.jar $IDENTITES_OPT $CONFIG_OPT ${ISPN_HOME}/server/conf
 
 if [ -n "${DEBUG}" ]; then
   cat ${SERVER_ROOT}/server/conf/*.xml
