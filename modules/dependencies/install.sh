c6a2d9609ea22958685b4433c65e33841bcdcf53
Osni Oliveira <osni.oliveira@redhat.com>
2020-01-14 12:53:31 -0300

    Do not delete rpm/dnf repos info, needed for meta
    There was a size optimization (8MB) deleting /var/lib/rpm and /var/lib/dnf inside the container.
    However, this information is used by the all_rpm_packages plugin in Brew, in order to generate meta information for Lightblue (container grading, CVEs)
    
    Signed-off-by: Osni Oliveira <osni.oliveira@redhat.com>

--- modules/dependencies/install.sh
+++ modules/dependencies/install.sh
@@ -6,8 +6,5 @@ ADDED_DIR=$(dirname $0)/added
 # Override default java.config file so that tls is not disabled
 cp $ADDED_DIR/java.config /etc/crypto-policies/back-ends/java.config
 
-# Remove dnf and rpm files as package manager not required at runtime
-rm -rf /var/lib/rpm /var/lib/dnf
-
 # Create symlink between bsdtar and tar to enable `oc copy`
 ln -s $(command -v bsdtar) /usr/bin/tar
