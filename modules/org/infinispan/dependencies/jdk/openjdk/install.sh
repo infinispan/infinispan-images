8a759fc15fb91d8e307f2211f6d19ea70e41294d
Ryan Emerson <remerson@redhat.com>
2020-05-05 12:44:10 +0100

    Add common jdk module to prevent duplication of security config

--- modules/org/infinispan/dependencies/jdk/openjdk/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-ADDED_DIR=$(dirname $0)/added
-
-# Override default java.config file so that tls is not disabled
-cp $ADDED_DIR/java.config /etc/crypto-policies/back-ends/java.config
