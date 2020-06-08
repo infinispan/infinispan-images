e97da522fa9a3dfcaffaa12ed5177b22e8f5f8f9
Ryan Emerson <remerson@redhat.com>
2020-06-08 10:39:34 +0100

    Add .jar extension if config-generator is not a native binary

--- modules/org/infinispan/distribution/jvm/extract.sh
+++ modules/org/infinispan/distribution/jvm/extract.sh
@@ -8,5 +8,13 @@ mkdir -p $SERVER_ROOT
 cd $SERVER_ROOT
 bsdtar --strip-components=1 -xvf /tmp/artifacts/server
 
-cp -r /tmp/artifacts/config-generator* $ADDED_DIR/bin/* $SERVER_ROOT/bin
+if jar -tf /tmp/artifacts/config-generator 2>&1 /dev/null; then
+    # If the provided config-generator artifact is a valid jar, then add the extension so that we know to execute on
+    # the JVM in bin/launch.sh
+    cp /tmp/artifacts/config-generator $SERVER_ROOT/bin/config-generator.jar
+else
+    cp /tmp/artifacts/config-generator $SERVER_ROOT/bin
+fi
+
+cp -r $ADDED_DIR/bin/* $SERVER_ROOT/bin
 rm $SERVER_ROOT/server/conf/infinispan-local.xml
