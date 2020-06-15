09a84c264a35cd5df926e77e86fa547d279269f3
Ryan Emerson <remerson@redhat.com>
2020-06-15 15:56:39 +0100

    Native server: Don't compile test sources

--- modules/quarkus/server/compile.sh
+++ modules/quarkus/server/compile.sh
@@ -4,5 +4,5 @@ set -e
 mkdir -p /opt/build
 cd /opt/build
 tar --strip-components=1 -xvf /tmp/artifacts/server-src.tar.gz
-mvn clean install -Dnative -DskipTests=true
+mvn clean install -Dnative -DskipTests -am -pl server-runner
 cp server-runner/target/infinispan-quarkus-server-runner-*-runner /opt/server
