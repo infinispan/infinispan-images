05dae8403e8bc5e45be1ffcfd3d374a4186d8ae3
Ryan Emerson <remerson@redhat.com>
2021-01-25 13:01:03 +0000

    Update CLI binary name

--- modules/quarkus/infinispan/cli/compile.sh
+++ modules/quarkus/infinispan/cli/compile.sh
@@ -3,4 +3,4 @@ set -e
 
 cd /opt/build
 mvn clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Dnative -DskipTests -pl 'cli'
-cp /opt/build/cli/target/infinispan-quarkus-cli-*-runner /opt/cli
+cp /opt/build/cli/target/ispn-cli /opt/cli
