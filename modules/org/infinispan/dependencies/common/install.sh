aefb2ead28c93ec969e5dcf0a4db8d474b896cdc
Ryan Emerson <remerson@redhat.com>
2021-05-19 14:39:06 +0100

    Utilise ubi8/openjdk-11-runtime as base for infinispan/server image

--- modules/org/infinispan/dependencies/common/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create symlink between bsdtar and tar to enable `oc copy`
-ln -s $(command -v bsdtar) /usr/bin/tar
--- modules/org/infinispan/distribution/jvm/extract.sh
+++ modules/org/infinispan/distribution/jvm/extract.sh
@@ -8,7 +8,7 @@ mkdir -p $SERVER_ROOT
 cd $SERVER_ROOT
 bsdtar --strip-components=1 -xvf /tmp/artifacts/server
 
-if jar -tf /tmp/artifacts/config-generator 2>&1 /dev/null; then
+if file --mime-type /tmp/artifacts/config-generator | grep -q "application/zip"; then
     # If the provided config-generator artifact is a valid jar, then add the extension so that we know to execute on
     # the JVM in bin/launch.sh
     cp /tmp/artifacts/config-generator $SERVER_ROOT/bin/config-generator.jar
