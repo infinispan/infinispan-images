6a2bf9aef3cf31ccdf915120291221f39942a892
Ryan Emerson <remerson@redhat.com>
2023-04-26 14:11:52 +0100

    Constrain server native xmx

--- modules/quarkus/infinispan/server/compile.sh
+++ modules/quarkus/infinispan/server/compile.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd /opt/build
-./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Pnative -Pdistribution -DskipTests -am -pl quarkus/server/deployment,quarkus/server-runner
+./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Pnative -Pdistribution -DskipTests -am -pl quarkus/server/deployment,quarkus/server-runner -Dquarkus.native.native-image-xmx=8g
 cp /opt/build/quarkus/server-runner/target/infinispan-quarkus-server-runner-*-runner /opt/server
