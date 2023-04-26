7678628f7f2e3b45efd1a9f7f058d87cad292fff
Ryan Emerson <remerson@redhat.com>
2023-04-26 10:36:03 +0100

    Build native components from main infinispan repository

--- modules/quarkus/infinispan/server/compile.sh
+++ modules/quarkus/infinispan/server/compile.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd /opt/build
-./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Dnative -DskipTests -pl '!cli,!integration-tests,!integration-tests/embedded,!integration-tests/server'
-cp /opt/build/server-runner/target/infinispan-quarkus-server-runner-*-runner /opt/server
+./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Pnative -Pdistribution -DskipTests -am -pl quarkus/server/deployment,quarkus/server-runner
+cp /opt/build/quarkus/server-runner/target/infinispan-quarkus-server-runner-*-runner /opt/server
