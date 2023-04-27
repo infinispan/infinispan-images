18faabe565c39fbb575aba19a40f861d2e8e7e1d
Ryan Emerson <remerson@redhat.com>
2023-04-27 12:27:01 +0100

    Constrain CLI xmx

--- modules/quarkus/infinispan/cli/compile.sh
+++ modules/quarkus/infinispan/cli/compile.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd /opt/build
-./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Pnative -Pdistribution -DskipTests -am -pl 'quarkus/cli'
+./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Pnative -Pdistribution -DskipTests -am -pl 'quarkus/cli' -Dquarkus.native.native-image-xmx=8g
 cp /opt/build/quarkus/cli/target/infinispan-cli /opt/cli
