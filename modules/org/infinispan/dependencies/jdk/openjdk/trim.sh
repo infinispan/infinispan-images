b5e38ab40e06b8c32693f680467ff54462eb5614
Priyanka Minz <pminz@redhat.com>
2023-05-03 11:33:37 +0530

    update trim.sh script to check packages

--- modules/org/infinispan/dependencies/jdk/openjdk/trim.sh
+++ modules/org/infinispan/dependencies/jdk/openjdk/trim.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-set -e
+#set -e
 DIR=$(dirname $0)
 PACKAGES=$(cat $DIR/packages.txt)
-rpm -e --nodeps $PACKAGES
+for pkg in $PACKAGES
+do
+    cmd=`rpm --verify $pkg 2> /dev/null`
+    rc=$?
+    if [[ $rc -eq 0 ]]
+    then
+        echo "${pkg} exists"
+        rpm -e --nodeps $pkg
+    else
+        echo "${pkg} does not exist"
+    fi
+done
