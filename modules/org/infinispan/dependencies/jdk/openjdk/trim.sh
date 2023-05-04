c099d613e94ec7907c1e11903e859a6f8708f04d
Priyanka Minz <pminz@redhat.com>
2023-05-04 15:27:28 +0530

    update trim.sh script to verify packages

--- modules/org/infinispan/dependencies/jdk/openjdk/trim.sh
+++ modules/org/infinispan/dependencies/jdk/openjdk/trim.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#set -e
+set -e
 DIR=$(dirname $0)
 PACKAGES=$(cat $DIR/packages.txt)
 for pkg in $PACKAGES
 do
-    cmd=`rpm --verify $pkg 2> /dev/null`
-    rc=$?
-    if [[ $rc -eq 0 ]]
+    if rpm --verify $pkg 2> /dev/null 
     then
-        echo "${pkg} exists"
         rpm -e --nodeps $pkg
-    else
-        echo "${pkg} does not exist"
     fi
 done
