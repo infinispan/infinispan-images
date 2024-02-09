26086cef560967e4ef8ee15677d087f49be6a39a
Ryan Emerson <remerson@redhat.com>
2024-02-09 10:10:32 +0000

    Download Infinispan artifacts from Maven instead of building locally

--- modules/quarkus/infinispan/cli/compile.sh
+++ modules/quarkus/infinispan/cli/compile.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd /opt/build
-./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Dnative -DskipTests -pl 'cli'
+./mvnw package -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Dnative -DskipTests -pl 'cli'
 cp /opt/build/cli/target/infinispan-cli /opt/cli
--- modules/quarkus/infinispan/server/compile.sh
+++ modules/quarkus/infinispan/server/compile.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd /opt/build
-./mvnw clean install -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Dnative -DskipTests -pl '!cli,!integration-tests,!integration-tests/embedded,!integration-tests/server'
+./mvnw package -s /tmp/scripts/quarkus.infinispan.src/maven-settings.xml -Dnative -DskipTests -pl '!cli,!integration-tests,!integration-tests/embedded,!integration-tests/server'
 cp /opt/build/server-runner/target/infinispan-quarkus-server-runner-*-runner /opt/server
