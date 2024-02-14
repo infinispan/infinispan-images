63649d6361e7d3bf17c17475ae1791cb837d94ac
Ryan Emerson <remerson@redhat.com>
2024-02-14 13:36:05 +0000

    Set J-XX:GCTimeRatio=99 when compiling native CLI and server

--- modules/quarkus/infinispan/cli/compile.sh
+++ modules/quarkus/infinispan/cli/compile.sh
@@ -9,6 +9,7 @@ cd /opt/build
     -DskipTests \
     -pl 'quarkus/cli' \
     -Dquarkus.native.native-image-xmx=8g \
+    -Dquarkus.native.additional-build-args=-J-XX:GCTimeRatio=99 \
     -Dmaven.buildNumber.revisionOnScmFailure=no-scm
 
 cp /opt/build/quarkus/cli/target/infinispan-cli /opt/cli
