1ae50f884ffb42b257d80163c3004f07474cf660
Ryan Emerson <remerson@redhat.com>
2024-03-11 17:41:28 +0000

    Migrate CI and release scripts to GitHub Actions
    Descriptor files migrated to yq formatting to ensure that the diff of
    the first release commits only contains release related changes.

---
+++ .github/scripts/image_tags.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  set -x
+fi
+
+function requiredEnv() {
+  for ENV in $@; do
+      if [ -z "${!ENV}" ]; then
+        echo "${ENV} variable must be set"
+        exit 1
+      fi
+  done
+}
+
+requiredEnv IMAGE_VERSION DESCRIPTOR LATEST
+
+if [[ "${DESCRIPTOR}" == "server-openjdk" ]]; then
+  IMAGE="server"
+else
+  IMAGE=${DESCRIPTOR}
+fi
+
+ISPN_VERSION=${IMAGE_VERSION%-*}
+MAJOR_MINOR_VERSION=${ISPN_VERSION%.*.*}
+
+TAGS="${MAJOR_MINOR_VERSION} ${ISPN_VERSION} ${IMAGE_VERSION}"
+if [[ "${LATEST}" == "true" ]]; then
+  TAGS+=" latest"
+fi
+
+FQ_TAGS=""
+for REPO in "quay.io/" ""; do
+  for TAG in ${TAGS}; do
+    FQ_TAGS+="${REPO}infinispan/${IMAGE}:${TAG},"
+  done
+done
+echo ${FQ_TAGS::-1}
---
+++ .github/scripts/update_image_descriptors.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ "$RUNNER_DEBUG" == "1" ]]; then
+  set -x
+fi
+
+function requiredEnv() {
+  for ENV in $@; do
+      if [ -z "${!ENV}" ]; then
+        echo "${ENV} variable must be set"
+        exit 1
+      fi
+  done
+}
+
+function nextImageVersion() {
+    BUILD=1
+    if [[ "${CURRENT_VERSION}" == ${ISPN_VERSION}* ]]; then
+        BUILD=$((BUILD+1))
+    fi
+    echo ${ISPN_VERSION}-${BUILD}
+}
+
+requiredEnv ISPN_VERSION DESCRIPTOR
+
+YAML="${DESCRIPTOR}.yaml"
+if [[ "${DESCRIPTOR}" == "server-openjdk" ]]; then
+    # Single stage build
+    CURRENT_VERSION=$(yq '.version' ${YAML})
+    IMAGE_VERSION=$(nextImageVersion)
+    yq -i ".version = \"${IMAGE_VERSION}\"" ${YAML}
+    yq -i ".artifacts[0].url = \"https://downloads.jboss.org/infinispan/${ISPN_VERSION}/infinispan-server-${ISPN_VERSION}.zip\"" ${YAML}
+    yq -i "(.labels[] | select(.name == \"version\" or .name == \"release\") | .value) = \"${ISPN_VERSION}\"" ${YAML}
+else
+    # Multi-stage build
+    CURRENT_VERSION=$(yq '.[0].version' ${YAML})
+    IMAGE_VERSION=$(nextImageVersion)
+
+    # Update builder descriptor
+    yq -i ".[0].version = \"${IMAGE_VERSION}\"" ${YAML}
+    yq -i ".[0].artifacts[0].url = \"https://github.com/infinispan/infinispan/archive/${ISPN_VERSION}.tar.gz\"" ${YAML}
+
+    # Update runner descriptor
+    yq -i ".[1].version = \"${IMAGE_VERSION}\"" ${YAML}
+    yq -i "(.[1].labels[] | select(.name == \"version\" or .name == \"release\") | .value) = \"${ISPN_VERSION}\"" ${YAML}
+fi
+echo "${IMAGE_VERSION}"
