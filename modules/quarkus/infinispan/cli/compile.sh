bbe8d043b61cbdbb50505fe72f02a5102232c2e9
Ryan Emerson <remerson@redhat.com>
2025-03-27 12:19:01 +0000

    Skip git-commit-id-maven-plugin when building CLI from src

--- modules/quarkus/infinispan/cli/compile.sh
+++ modules/quarkus/infinispan/cli/compile.sh
@@ -10,6 +10,7 @@ cd /opt/build
     -Dcheckstyle.skip \
     -pl 'quarkus/cli' \
     -Dquarkus.native.native-image-xmx=8g \
-    -Dmaven.buildNumber.revisionOnScmFailure=no-scm
+    -Dmaven.buildNumber.revisionOnScmFailure=no-scm \
+    -Dmaven.gitcommitid.skip=true
 
 cp /opt/build/quarkus/cli/target/infinispan-cli /opt/cli
